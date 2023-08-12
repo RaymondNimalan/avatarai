511edb91fca540d3351e125126f269e17659254d
Ray Nimalan <raymondnimalan@gmail.com>
2023-08-11 20:18:33 -0400

    feat: new landing comp

---
+++ src/components/Landing.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const Landing = () => {
+    const getAvatar = async () => {
+        console.log("INSIDE FETCH REQ")
+
+    }
+  return (
+    <div>Landing</div>
+  )
+}
+
+export default Landing

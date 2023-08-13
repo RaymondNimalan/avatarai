60541c8310c6d26d288b94cb9d3a04ad6b2a0b88
Ray Nimalan <raymondnimalan@gmail.com>
2023-08-12 23:15:27 -0400

    fix: landing folder structure

---
+++ src/components/landing/Landing.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { LandingContainer } from './landing_styles';
+
+const Landing = () => {
+
+
+  useEffect(() => {
+    console.log('useEffect');
+    getAvatar()
+  }, []);
+    const getAvatar = async () => {
+        console.log("INSIDE FETCH REQ")
+        const request = await fetch('http://localhost:8000/')
+        const message = await request.json()
+        console.log('request', message)
+        return message
+       }
+
+  return (
+    <LandingContainer>
+        <div>Landing</div>
+    </LandingContainer>
+  )
+}
+
+export default Landing
